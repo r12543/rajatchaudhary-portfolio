ba0cdf012aa868cb1ca790c99ee8004198cf6cef
Rajat Chaudhary <chaudharyrajat80@gmail.com>
2020-04-17 11:51:31 +0800

    Add basic files

---
+++ src/components/portfolio/portfolio.js
@@ -0,0 +1,49 @@
+
+import React, { Component } from 'react';
+export default class Header extends Component {
+  render() {
+    return (
+      <React.Fragment>
+      {/*generated code*/}
+      <section id="portfolio">
+        <div className="row">
+          <div className="twelve columns collapsed">
+            <h1>Check Out Some of My Works.</h1>
+            {/* portfolio-wrapper */}
+            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+              <div className="columns portfolio-item">
+                <div className="item-wrap">
+                  <a href="#modal-01" title>
+                    <img alt="" src="images/portfolio/coffee.jpg" />
+                    <div className="overlay">
+                      <div className="portfolio-item-meta">
+                        <h5>Coffee</h5>
+                        <p>Illustrration</p>
+                      </div>
+                    </div>
+                    <div className="link-icon"><i className="icon-plus" /></div>
+                  </a>
+                </div>
+              </div> {/* item end */}
+            </div> {/* portfolio-wrapper end */}
+          </div> {/* twelve columns end */}
+          {/* Modal Popup
+	      --------------------------------------------------------------- */}
+          <div id="modal-01" className="popup-modal mfp-hide">
+            <img className="scale-with-grid" src="images/portfolio/modals/m-coffee.jpg" alt="" />
+            <div className="description-box">
+              <h4>Coffee Cup</h4>
+              <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
+              <span className="categories"><i className="fa fa-tag" />Branding, Webdesign</span>
+            </div>
+            <div className="link-box">
+              <a href="http://www.behance.net">Details</a>
+              <a className="popup-modal-dismiss">Close</a>
+            </div>
+          </div>{/* modal-01 End */}
+        </div> {/* row End */}
+      </section> {/* Portfolio Section End*/}
+      </React.Fragment>
+    );
+  }
+}
---
+++ src/data.js
@@ -0,0 +1,6 @@
+let data = {
+    "imagebaseurl":"http://localhost:3000/",
+    "name": "Rajat Chaudhary",
+    "role": "Data Engineer"
+}
+export default data
