d9b95a5186dbaaa6fcbbf23280d26d09b0f3d53e
Rajat Chaudhary <chaudharyrajat80@gmail.com>
2020-04-30 17:33:37 +0800

    add build and gcp files

---
+++ build/precache-manifest.20d4bd926ae973b4dc294f98acb5d563.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "645ff5af639ef7f572fba95b9e99a6d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c80863a2b6bb1cbb066",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d50b41ce7abd7cbcd713",
+    "url": "/static/js/2.1dc7a1fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1dc7a1fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c80863a2b6bb1cbb066",
+    "url": "/static/js/main.11a80ed2.chunk.js"
+  },
+  {
+    "revision": "837e7a9c8851833f5c35",
+    "url": "/static/js/runtime-main.fc9351cb.js"
+  }
+]);
